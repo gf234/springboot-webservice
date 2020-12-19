0db185a94af9c1727d47f181ee10b8280680effb
Seonggu Lee <gf265@naver.com>
2020-12-19 18:45:31 +0900

    add index, save, update

---
+++ src/test/java/com/example/springbootwebservice/web/dto/IndexControllerTest.java
@@ -0,0 +1,25 @@
+package com.example.springbootwebservice.web.dto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void main_page_loading(){
+        // when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        // then
+        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+    }
+}
