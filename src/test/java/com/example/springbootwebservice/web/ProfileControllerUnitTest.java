2055f3c439942bc6749efe963fc60f529bb3d5ca
Seonggu Lee <gf265@naver.com>
2020-12-30 18:51:41 +0900

    refactor test code

--- src/test/java/com/example/springbootwebservice/web/ProfileControllerUnitTest.java
+++ src/test/java/com/example/springbootwebservice/web/ProfileControllerUnitTest.java
@@ -1,6 +1,6 @@
 package com.example.springbootwebservice.web;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.mock.env.MockEnvironment;
 
 import static org.junit.jupiter.api.Assertions.*;
