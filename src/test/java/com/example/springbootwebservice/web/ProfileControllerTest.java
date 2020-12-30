f0ccdb8a90e7523a78a9d7b46a32f08dd8d7c4ea
Seonggu Lee <gf265@naver.com>
2020-12-30 19:10:55 +0900

    refactor test code

--- src/test/java/com/example/springbootwebservice/web/ProfileControllerTest.java
+++ src/test/java/com/example/springbootwebservice/web/ProfileControllerTest.java
@@ -1,19 +1,19 @@
 package com.example.springbootwebservice.web;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProfileControllerTest {
 
